2284eb1c76e69df80726c9240f8c4df6f3c247ba
Darius K <dariuskumza@gmail.com>
2023-10-09 16:39:12 +0300

    content added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: ["**/*.{html,js}"],
+   content: ["*.{html,js}"],
    theme: {
       extend: {},
+      screens: {
+         // cutting out TWCSS default values
+         sm: { min: "1023px", max: "1023px" },
+         // => @media (min-width: 640px and max-width: 767px) { ... }
+
+         md: { min: "1023px", max: "1023px" },
+         // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+         lg: { min: "1023px", max: "1023px" },
+         // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+         xl: { min: "1023px", max: "1023px" },
+         // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+         "2xl": { min: "1536px" },
+         // => @media (min-width: 1536px) { ... }
+      },
    },
    plugins: [],
 };
