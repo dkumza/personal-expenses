3314c8f076974225674f0ac4b43ed847fcd95afe
Darius K <dariuskumza@gmail.com>
2023-10-10 14:54:31 +0300

    styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,19 +9,19 @@ module.exports = {
       },
       screens: {
          // cutting out TWCSS default values
-         sm: { min: "700px", max: "700px" },
+         sm: { min: "1024px", max: "1024px" },
          // => @media (min-width: 640px and max-width: 767px) { ... }
 
-         md: { min: "700px", max: "700px" },
+         md: { min: "1024px", max: "1024px" },
          // => @media (min-width: 768px and max-width: 1023px) { ... }
 
-         lg: { min: "700px", max: "700px" },
+         lg: { min: "1024px", max: "1024px" },
          // => @media (min-width: 1024px and max-width: 1279px) { ... }
 
-         xl: { min: "700px", max: "700px" },
+         xl: { min: "1024px", max: "1024px" },
          // => @media (min-width: 1280px and max-width: 1535px) { ... }
 
-         "2xl": { min: "700px" },
+         "2xl": { min: "1024px" },
          // => @media (min-width: 1536px) { ... }
       },
    },
