767e138c3e6e7fc6f293f563b9aec795e85c5db9
Darius K <dariuskumza@gmail.com>
2023-10-12 20:15:48 +0300

    modal added and working

--- main.js
+++ main.js
@@ -1,3 +1,42 @@
+// modal for transaction history
+
+// declare variables
+let historyBtn = document.querySelector(".btn-history");
+let modal1 = document.querySelector(".add-blur");
+let historyExpanded = document.querySelector(".history-expanded");
+
+// show modal
+historyBtn.addEventListener("click", () => {
+   modal1.classList.remove("hide");
+   historyExpanded.classList.remove("hide");
+});
+
+// hide modal
+
+modal1.addEventListener("click", () => {
+   modal1.classList.add("hide");
+   historyExpanded.classList.add("hide");
+});
+// end of transaction history modal
+
+// add transaction modal
+let modal2 = document.querySelector(".add-blur");
+let transactionBtn = document.querySelector(".btn");
+let transactionWrap = document.querySelector(".transaction-wrap");
+let addTransaction = document.querySelector(".add-transactions");
+
+transactionBtn.addEventListener("click", () => {
+   modal2.classList.remove("hide");
+   transactionWrap.classList.remove("hide");
+});
+
+modal2.addEventListener("click", () => {
+   modal2.classList.add("hide");
+   addTransaction.classList.add("hide");
+});
+
+// let modal2 = doc;
+
 // chart.js
 
 let xLabel = ["Food", "Healtcare", "Housing"];
@@ -11,7 +50,7 @@ let barColors = [
 ];
 
 new Chart("myChart", {
-   type: "polarArea",
+   type: "pie",
    data: {
       labels: xLabel,
       datasets: [
@@ -26,41 +65,6 @@ new Chart("myChart", {
       title: {
          display: true,
          //  text: "Expanses",
-      },
-      legend: {
-         display: true,
-         position: "bottom",
-      },
-   },
-});
-
-let xLabel1 = ["Salary", "Healtcare", "Housing"];
-let yLabel1 = [200, 100, 500];
-let barColors1 = [
-   "rgb(255, 99, 132)", // Food
-   "rgb(75, 192, 192)", // Healthcare
-   "rgb(255, 205, 86)", // Housing
-   "rgb(99, 135, 255)", // Salary
-   //    "rgb(54, 162, 235)",
-];
-
-new Chart("myChart1", {
-   type: "polarArea",
-   data: {
-      labels: xLabel1,
-      datasets: [
-         {
-            backgroundColor: barColors1,
-            data: yLabel1,
-            // borderColor:
-         },
-      ],
-   },
-   options: {
-      responsive: false,
-      title: {
-         display: true,
-         text: "Income",
       },
       legend: {
          display: true,
